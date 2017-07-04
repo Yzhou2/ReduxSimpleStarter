fbe70f66a266cfde0c7f59614b6c9dc74e119d5a
jessica <samandjesslovepie@gmail.com>
2017-07-04 16:06:12 -0600

    youtube video browser-react-search_bar-component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCuEXQR9gMiHK4VeUsHzxbMQuYiYz60B6I';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    render {
+      return (
+        <div><SearchBar /></div>
+      )
+    }
+
+  }
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
